243a77d0ee5a6afd6005fcfd318f8c9797332a7e
Sepideh Jahangirzadeh <sepidehjahangirzadeh@gmail.com>
2026-06-03 12:11:32 +0330

    modify app/core/__init__.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,2 +1,3 @@
 from .config import settings
-from.exceptions import LLMGenerationError, StructuredOutputError, VectorSearchError, ElasticFailure
+from .exceptions import LLMGenerationError, StructuredOutputError, VectorSearchError, ElasticFailure
+from .elastic_log_handler import ElasticLogHandler
